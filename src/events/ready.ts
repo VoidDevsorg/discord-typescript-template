adb60b7dfea675c39eadd6cc34a049599b3e8953
Bunny <76551996+clqu@users.noreply.github.com>
2023-06-06 07:48:14 +0300

    Update ready.ts
    Fix event type

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
-import { Client } from "discord.js";
+import { IEvent } from "../interfaces";
 
-export default {
+export const Event: IEvent = {
     name: 'ready',
     run: (client) => {
         console.success(`Logged in as ${client.user.tag}!`);
